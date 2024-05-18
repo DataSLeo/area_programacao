3352c41e2cf30a87f17f88d3ebf1ac272a6b369f
Leonardo Pereira <leonardo.alves.c155@gmail.com>
2024-05-18 18:10:36 -0300

    2

---
+++ Java/Estudos/Java/02/App.java
@@ -0,0 +1,15 @@
+public class App{
+	public static void main (String[] args){
+
+		int age = 20;
+		String name = "Leonardo";
+		boolean student = true;
+		float money = 0.0f;
+
+		System.out.println("Age: " + age);
+		System.out.println("Name: " + name);
+		System.out.println("Student: " + student);
+		System.out.println("Money " + money);
+
+	}
+}
