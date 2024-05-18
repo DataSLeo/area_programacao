0b2e2eefd12924a8a77c51d073eb6c8f1cd8929f
Leonardo Pereira <leonardo.alves.c155@gmail.com>
2024-05-18 17:35:41 -0300

    1

---
+++ Java/Estudos/Java/hello_world/App.java
@@ -0,0 +1,13 @@
+// Class app
+public class App {
+
+	// The main
+	public static void main (String args[]) {
+
+		// Printing 'hello, world' and next printing 'I am another print'
+		System.out.println("Hello, world!");
+		System.out.print("I am another print");
+
+	}
+
+} 
